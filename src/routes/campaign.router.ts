ea0e36f1f3ce62d4b4844f7a64bad80b14cebbd8
sanjaymurmu1 <sanjay@hunch.in>
2025-04-10 23:18:39 +0530

    router post for campaign

--- src/routes/campaign.router.ts
+++ src/routes/campaign.router.ts
@@ -1,9 +1,17 @@
 import { Router } from 'express';
 import { CampaignController } from '../controllers/campaign.controller';
+import { Request, Response, NextFunction } from 'express';
 
 const router = Router();
 const campaignController = new CampaignController();
 
-router.post('/', campaignController.createCampaign);
+router.post('/', async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    await campaignController.createCampaign(req, res);
+  } catch (error) {
+    next(error);
+  }
+});
 
 export default router;
+
